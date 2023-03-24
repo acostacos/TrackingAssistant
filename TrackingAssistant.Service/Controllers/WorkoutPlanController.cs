e2f8efce16b70a776441999ae41c84fcdc6ce114
Carlo Acosta <carlo.acosta@infor.com>
2023-03-24 14:16:05 +0800

    Renamed Services. Added Application Library. Made bolier plate files for Workout Plan

---
+++ TrackingAssistant.Service/Controllers/WorkoutPlanController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrackingAssistant.Controllers
+{
+    // TODO: Study Program.cs class first and what each method does
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutPlanController : ControllerBase
+    {
+        public WorkoutPlanController()
+        {
+            // Figure out how to do dependency injection
+        }
+
+        // Figure out what information you need to put here
+        [HttpGet]
+        public IActionResult GetWorkoutPlan()
+        {
+            return Ok();
+            //return NotFound(); if not found
+        }
+
+        [HttpPost]
+        public IActionResult CreateWorkoutPlan()
+        {
+            return Ok();
+            //return Created(); default response?
+            //return Conflict(); if already exists
+        }
+
+        // Check difference between put and patch
+        [HttpPatch]
+        public IActionResult UpdateWorkoutPlan()
+        {
+            return Ok();
+            //return NotFound();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteWorkoutPlan()
+        {
+            return Ok();
+        }
+    }
+}
